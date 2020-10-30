66f184969b9131f8f4e5a7d7664b64d67844e73e
Richard Walters <rwalters@digitalstirling.com>
2020-10-29 19:32:50 -0700

    Work in progress adding connection processor code

--- src/lib.rs
+++ src/lib.rs
@@ -70,45 +70,126 @@ enum ListenerMessage {
 
 async fn accept_connections(
     mut listener_receiver: mpsc::UnboundedReceiver<ListenerMessage>,
+    connection_sender: mpsc::UnboundedSender<Box<dyn Connection>>,
 ) {
+    // Listener is in a `RefCell` because two separate futures need to access
+    // it:
+    // * `accepter` takes the listener, uses it, and potentially puts it back.
+    // * `message_handler` places new listeners there (potentially dropping any
+    //   previous listeners that might still be there).
     println!("accept_connections: entered");
     let listener: RefCell<Option<TcpListener>> = RefCell::new(None);
     loop {
         let accepter = async {
+            // This future should only do something and complete if we have
+            // a listener.  If we don't, we need the other future to complete,
+            // and we have nothing to do here, so never complete.
             let current_listener = listener.borrow_mut().take();
             if let Some(current_listener) = current_listener {
+                // Here we wait on the listener for the next incoming
+                // connection from an HTTP client.  If we get a connection, we
+                // need to put the listener back so that when we loop back and
+                // "run this future again" (technically a new future is made
+                // with this same async block) we can take the listener back
+                // out.
                 println!("accept_connections: Accepting next connection");
                 if let Ok((connection, address)) = current_listener.accept().await {
                     println!("accept_connections: Connection received from {}", address);
                     listener.replace(Some(current_listener));
+                    connection_sender.unbounded_send(Box::new(connection));
                 } else {
                     // TODO: This happens if the listener breaks somehow.
                     // We should set up a mechanism for reporting this.
                 }
             } else {
+                // This is a fancy nerdy way of saying the following:
+                // "So, little future, guess what.  You're never going
+                // to escape, so just sleep here forever!"
                 futures::future::pending().await
             }
         };
         let message_handler = async {
-            // If there's no listener, there should be a kicker, so this
-            // shouldn't fail.  If it does, we have a bug and want a panic to
-            // see it while debugging.
+            // If `listener_receiver` completes (has no next), it means the
+            // sender end, which is held by `handle_messages`, was dropped
+            // somehow.  This can only really happen if there are no more
+            // listeners to receive and the `handle_messages` future has
+            // completed, which only happens if the worker thread overall is
+            // told to exit.  In that case, we don't want to loop trying to get
+            // the next message here, because it would either panic or
+            // constantly
             println!("accept_connections: Waiting for messages");
-            let message = listener_receiver.next().await;
-            match message.unwrap() {
-                ListenerMessage::Start(new_listener) => {
+            match listener_receiver.next().await {
+                Some(ListenerMessage::Start(new_listener)) => {
                     println!(
                         "accept_connections: Now accepting connections on port {}",
                         new_listener.local_addr().unwrap().port()
                     );
                     listener.replace(Some(new_listener));
                 },
+                None => futures::future::pending().await,
             }
         };
         futures::select!(
             () = accepter.fuse() => (),
             () = message_handler.fuse() => (),
         );
+    }
+}
+
+// This is the type of value returned by a completed future selected by
+// `accept_connections`.  It's used to tell the difference between a future
+// which processed a connection from a future which processed the connection
+// receiver.
+enum ProcessorKind {
+    Connection,
+    Receiver,
+}
+
+async fn handle_connections(
+    mut connection_receiver: mpsc::UnboundedReceiver<Box<dyn Connection>>,
+) {
+    let mut processors = Vec::new();
+    let mut needs_next_connection = true;
+    loop {
+        // Add a special future to receive the next connection.
+        if needs_next_connection {
+            let next_connection = async {
+                // If `connection_receiver` completes (has no next), it means
+                // the sender end, which is held by `accept_connections`, was
+                // dropped somehow.  This should never happen, since that
+                // future never completes.
+                let connection = connection_receiver.next().await.unwrap();
+
+                processors.push(async {
+                    // TODO: Here is where we will be receiving the request
+                    // from the HTTP client, sending it to a request handler,
+                    // and sending back a response (possibly in a loop).
+
+                    // The output indicates that this is the future used
+                    // to receive the next connection.
+                    ProcessorKind::Connection
+                }.boxed());
+
+                // The output indicates that this is the future used
+                // to receive the next connection.
+                ProcessorKind::Receiver
+            }.boxed();
+
+            // Add the "next connection" future to our collection.
+            processors.push(next_connection);
+        }
+
+        // Wait until a connection or the "connection receiver" completes.
+        let (processor_kind, _, processors_left) = future::select_all(
+            processors.into_iter()
+        ).await;
+
+        // If it was the "connection receiver" future which completed, mark
+        // that we will need to make a new one for the next loop.
+        needs_next_connection = matches!(processor_kind, ProcessorKind::Receiver);
+
+        // All incomplete futures go back to be collected next loop.
+        processors = processors_left;
     }
 }
 
@@ -155,8 +236,14 @@ async fn handle_messages(
 async fn worker(
     work_in_receiver: mpsc::UnboundedReceiver<WorkerMessage>
 ) {
-    // We will be in charge of the listener for incoming connections.
+    // These channels are used to pass values between the various
+    // sub-tasks below.
+    // * listener sender/receiver passes TcpListener values from
+    //   `handle_messages` to `accept_connections`.
+    // * connection sender/receiver passes boxed Connection values
+    //   from `accept_connections` to `handle_connections`.
     let (listener_sender, listener_receiver) = mpsc::unbounded();
+    let (connection_sender, connection_receiver) = mpsc::unbounded();
     futures::select!(
         // Handle incoming messages to the worker thread, such as to stop,
         // store a new value, or try to retrieve back a value previously
@@ -166,9 +253,9 @@ async fn worker(
             listener_sender
         ).fuse() => (),
 
-        () = accept_connections(listener_receiver).fuse() => (),
+        () = accept_connections(listener_receiver, connection_sender).fuse() => (),
 
-        // () = handle_requests()
+        () = handle_connections(connection_receiver).fuse() => (),
     );
 }
 
