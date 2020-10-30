23324a9103fb5aa7e126242c33d7bebc186b20a8
Richard Walters <rwalters@digitalstirling.com>
2020-10-30 16:25:41 -0700

    Drop unused Error::AlreadyStarted

--- src/error.rs
+++ src/error.rs
@@ -2,15 +2,6 @@
 /// crate generates.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    /// The server cannot be started because it's already running.  You'll need
-    /// to stop the server first by calling
-    /// [`stop`](struct.HttpServer.html#method.stop).
-    ///
-    /// TODO: We probably don't need this error, but let's keep it just in
-    /// case.
-    #[error("cannot start the server because it's already running")]
-    AlreadyStarted,
-
     /// The server cannot be stopped because it's not running.
     #[error("cannot stop the server because it's not running")]
     AlreadyStopped,
