52db339738ac8d193027d3704afeec848e1760ce
Richard Walters <rwalters@digitalstirling.com>
2020-10-30 16:24:52 -0700

    WIP implementing HTTP server
    * Implement request parsing and response generation.
    * Fix errors in handle_connections.
    * Replace all unwraps with expects so that a human-readable
      explanation of what happened is provided with any panic.
    
    TODO:
    
    * Connection reuse (if client didn't mark it to be closed,
      and connection is not to be upgraded)
    * Connection upgrades

--- src/error.rs
+++ src/error.rs
@@ -15,7 +15,30 @@ pub enum Error {
     #[error("cannot stop the server because it's not running")]
     AlreadyStopped,
 
+    /// The raw request received from the client could not be parsed.
+    #[error("unable to parse HTTP request")]
+    BadRequest(#[source] rhymuweb::Error),
+
+    /// The raw response to be sent to the client could not be turned into a
+    /// valid byte stream.
+    #[error("unable to generate HTTP response")]
+    BadResponse(#[source] rhymuweb::Error),
+
     /// The server could not bind or listen to the transport layer port.
     #[error("unable to bind or listen to the transport layer port")]
     Bind(#[source] std::io::Error),
+
+    /// The connection to the client was lost while awaiting a request.
+    #[error("disconnected from client")]
+    Disconnected,
+
+    /// The server encountered an error attempting to receive the
+    /// request from the client.
+    #[error("unable to receive request from client")]
+    UnableToReceive(#[source] std::io::Error),
+
+    /// The server encountered an error attempting to send the
+    /// response to the client.
+    #[error("unable to send response to client")]
+    UnableToSend(#[source] std::io::Error),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -29,10 +29,18 @@ use futures::{
 };
 use rhymuweb::{
     Request,
+    RequestParseStatus,
     Response,
 };
 use std::{
     cell::RefCell,
+    error::Error as _,
+    future::Future,
+    pin::Pin,
+    sync::{
+        Arc,
+        Mutex,
+    },
     thread,
 };
 
@@ -97,8 +105,18 @@ async fn accept_connections(
                         "accept_connections: Connection received from {}",
                         address
                     );
-                    listener.replace(Some(current_listener));
-                    connection_sender.unbounded_send(Box::new(connection));
+                    if listener.borrow().is_none() {
+                        listener.replace(Some(current_listener));
+                    }
+                    // This should never fail because the connection receiver
+                    // never completes.  Each connection will be at least
+                    // accepted by the processor future constructed to
+                    // handle it.
+                    connection_sender
+                        .unbounded_send(Box::new(connection))
+                        .expect(
+                            "connection dropped before it reached a processor",
+                        );
                 } else {
                     // TODO: This happens if the listener breaks somehow.
                     // We should set up a mechanism for reporting this.
@@ -124,7 +142,9 @@ async fn accept_connections(
                 Some(ListenerMessage::Start(new_listener)) => {
                     println!(
                         "accept_connections: Now accepting connections on port {}",
-                        new_listener.local_addr().unwrap().port()
+                        new_listener.local_addr().expect(
+                            "we should have been able to figure out our own address"
+                        ).port()
                     );
                     listener.replace(Some(new_listener));
                 },
@@ -144,59 +164,164 @@ async fn accept_connections(
 // receiver.
 enum ProcessorKind {
     Connection,
-    Receiver,
+    Receiver(mpsc::UnboundedReceiver<Box<dyn Connection>>),
+}
+
+type Processor = Pin<Box<dyn Future<Output = ProcessorKind> + Send>>;
+
+async fn receive_request(
+    mut connection: Box<dyn Connection>
+) -> Result<(Request, Box<dyn Connection>, Vec<u8>), Error> {
+    let mut request = Request::new();
+    let mut receive_buffer = Vec::new();
+    loop {
+        let left_over = receive_buffer.len();
+        receive_buffer.resize(left_over + 65536, 0);
+        let received = connection
+            .read(&mut receive_buffer[left_over..])
+            .await
+            .map_err(Error::UnableToReceive)
+            .and_then(|received| match received {
+                0 => Err(Error::Disconnected),
+                received => Ok(received),
+            })?;
+        receive_buffer.truncate(left_over + received);
+        let request_status =
+            request.parse(&mut receive_buffer).map_err(Error::BadRequest)?;
+        receive_buffer.drain(0..request_status.consumed);
+        if request_status.status == RequestParseStatus::Complete {
+            return Ok((request, connection, receive_buffer));
+        }
+    }
+}
+
+async fn handle_connection(
+    connection: Box<dyn Connection>
+) -> Result<Box<dyn Connection>, Error> {
+    // Assemble HTTP request from incoming data.
+    let (_, mut connection, _) = receive_request(connection).await?;
+
+    // Come up with a response and send it.
+    // TODO: Dispatch request to handler (use default "not
+    // found" handler if we can't find one) to produce an HTTP
+    // response.
+    //
+    // For now, just make a 404 "Not Found" response.
+    let mut response = Response::new();
+    response.status_code = 404;
+    response.reason_phrase = "Not Found".into();
+
+    // Send the HTTP response back through the connection.
+    let raw_response = response.generate().map_err(Error::BadResponse)?;
+    connection.write_all(&raw_response).await.map_err(Error::UnableToSend)?;
+    Ok(connection)
+}
+
+async fn await_next_connection(
+    mut connection_receiver: mpsc::UnboundedReceiver<Box<dyn Connection>>,
+    processors: Arc<Mutex<Vec<Processor>>>,
+) -> ProcessorKind {
+    // Wait for the next connection to come in.
+    //
+    // If `connection_receiver` completes (has no next), it means
+    // the sender end, which is held by `accept_connections`, was
+    // dropped somehow.  This should never happen, since that
+    // future never completes.
+    let next_connection = connection_receiver.next();
+    let connection = next_connection.await.expect(
+        "this task receives connections from other task which should never complete"
+    );
+    processors.lock().expect("last thread that held processors panicked").push(
+        async {
+            // Assemble HTTP request from incoming data.
+            match handle_connection(connection).await {
+                Ok(_) => {
+                    // TODO: We might want to hold onto the connection
+                    // in case the client is going to send another request.
+                },
+                Err(error) => {
+                    match error.source() {
+                        Some(source) => {
+                            eprintln!("error: {} ({})", error, source)
+                        },
+                        None => eprintln!("error: {}", error),
+                    };
+                },
+            }
+
+            // The output indicates that this is the future used
+            // to receive the next connection.
+            ProcessorKind::Connection
+        }
+        .boxed(),
+    );
+
+    // The output indicates that this is the future used
+    // to receive the next connection.
+    ProcessorKind::Receiver(connection_receiver)
 }
 
 async fn handle_connections(
-    mut connection_receiver: mpsc::UnboundedReceiver<Box<dyn Connection>>
+    connection_receiver: mpsc::UnboundedReceiver<Box<dyn Connection>>
 ) {
-    let mut processors = Vec::new();
+    let processors = Arc::new(Mutex::new(Vec::new()));
     let mut needs_next_connection = true;
+
+    // We need to wrap `connection_receiver` in an Option because we need to
+    // temporarily give it to a future (`handle_next_connection`) and receive
+    // `connection_receiver` back when that future completes.
+    let mut connection_receiver = Some(connection_receiver);
     loop {
         // Add a special future to receive the next connection.
         if needs_next_connection {
-            let next_connection = async {
-                // If `connection_receiver` completes (has no next), it means
-                // the sender end, which is held by `accept_connections`, was
-                // dropped somehow.  This should never happen, since that
-                // future never completes.
-                let connection = connection_receiver.next().await.unwrap();
-
-                processors.push(
-                    async {
-                        // TODO: Here is where we will be receiving the request
-                        // from the HTTP client, sending it to a request
-                        // handler, and sending back a
-                        // response (possibly in a loop).
-
-                        // The output indicates that this is the future used
-                        // to receive the next connection.
-                        ProcessorKind::Connection
-                    }
-                    .boxed(),
-                );
-
-                // The output indicates that this is the future used
-                // to receive the next connection.
-                ProcessorKind::Receiver
-            }
+            // `connection_receiver` should always be Some(connection)
+            // because we initialize it that way, and every time
+            // this future completes, we get it back from the future and
+            // put it back into `connection_receiver` again (since
+            // the future we're constructing here always completes
+            // with a value of `ProcessorKind::Receiver(connection)`.
+            let next_connection = await_next_connection(
+                connection_receiver.take().expect(
+                    "somehow we fumbled the connection receiver between connections"
+                ),
+                processors.clone(),
+            )
             .boxed();
 
             // Add the "next connection" future to our collection.
-            processors.push(next_connection);
+            processors
+                .lock()
+                .expect("last thread that held processors panicked")
+                .push(next_connection);
         }
 
         // Wait until a connection or the "connection receiver" completes.
-        let (processor_kind, _, processors_left) =
-            future::select_all(processors.into_iter()).await;
+        let processors_in = std::mem::take(
+            &mut *processors
+                .lock()
+                .expect("last thread that held processors panicked"),
+        );
+        let (processor_kind, _, processors_out) =
+            future::select_all(processors_in).await;
 
         // If it was the "connection receiver" future which completed, mark
         // that we will need to make a new one for the next loop.
-        needs_next_connection =
-            matches!(processor_kind, ProcessorKind::Receiver);
+        match processor_kind {
+            ProcessorKind::Receiver(new_connection_receiver) => {
+                connection_receiver.replace(new_connection_receiver);
+                needs_next_connection = true;
+            },
+            ProcessorKind::Connection => {
+                needs_next_connection = false;
+            },
+        }
 
         // All incomplete futures go back to be collected next loop.
-        processors = processors_left;
+        // We may have received new ones too, so combine them.
+        processors
+            .lock()
+            .expect("last thread that held processors panicked")
+            .extend(processors_out);
     }
 }
 
@@ -220,17 +345,23 @@ async fn handle_messages(
                     listener,
                     result_sender,
                 } => {
+                    // It's possible for the `send` here to fail, if the user
+                    // of the library gave up waiting for the result of
+                    // starting the server.  In that case we just drop
+                    // the result, since obviously they didn't care about it.
                     result_sender
                         .send({
                             println!(
                                 "handle_messages: Now listening for connections on port {}.",
-                                listener.local_addr().unwrap().port()
+                                listener.local_addr().expect(
+                                    "we should have been able to figure out our own address"
+                                ).port()
                             );
                             // This should never fail because `accept_connections`,
                             // which holds the receiver, never drops the receiver.
                             listener_sender
                                 .unbounded_send(ListenerMessage::Start(listener))
-                                .unwrap();
+                                .expect("listener dropped before it reached the connection accepter");
                             Ok(())
                         })
                         .unwrap_or(());
@@ -308,7 +439,7 @@ impl HttpServer {
             .map_err(Error::Bind)?;
         let (result_sender, result_receiver) = oneshot::channel();
         // It shouldn't be possible for this to fail, since the worker holds
-        // the receiver for this channel, and isn't dropped until the client
+        // the receiver for this channel, and isn't dropped until the server
         // itself is dropped.  So if it does fail, we want to know about it
         // since it would mean we have a bug.
         self.work_in
@@ -316,12 +447,14 @@ impl HttpServer {
                 listener,
                 result_sender,
             })
-            .unwrap();
+            .expect("worker message dropped before it could reach the worker");
         // It shouldn't be possible for this to fail, since the worker will
         // always send us back an answer; it should never drop the sender of
         // the channel before doing so.  So if it does fail, we want to know
         // about it since it would mean we have a bug.
-        result_receiver.await.unwrap()
+        result_receiver
+            .await
+            .expect("unable to receive result back from worker")
     }
 }
 
@@ -339,12 +472,18 @@ impl Drop for HttpServer {
         // the receiver for this channel, and we haven't joined or dropped the
         // worker yet (we will a few lines later).  So if it does fail, we want
         // to know about it since it would mean we have a bug.
-        self.work_in.unbounded_send(WorkerMessage::Exit).unwrap();
+        self.work_in
+            .unbounded_send(WorkerMessage::Exit)
+            .expect("worker message dropped before it could reach the worker");
 
         // Join the worker thread.
         //
         // This shouldn't fail unless the worker panics.  If it does, there's
         // no reason why we shouldn't panic as well.
-        self.worker.take().unwrap().join().unwrap();
+        self.worker.take().expect(
+            "somehow the worker thread join handle got lost before we could take it"
+        ).join().expect(
+            "the worker thread panicked before we could join it"
+        );
     }
 }
